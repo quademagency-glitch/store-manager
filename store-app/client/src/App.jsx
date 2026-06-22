6bd537d835e70e4cb606801684a109bab8520366
Quadem Digital <quadem.agency@gmail.com>
2026-06-22 00:42:14 +0000

    feat: integrate landing page route

--- store-app/client/src/App.jsx
+++ store-app/client/src/App.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from './lib/ThemeContext';
 import { ToastProvider } from './hooks/useToast';
 import { ConfirmProvider } from './hooks/useConfirm';
 import ProtectedRoute from './components/ProtectedRoute';
+import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
@@ -73,6 +74,7 @@ export default function App() {
             <AuthProvider>
               <ErrorBoundary>
                 <Routes>
+                  <Route path="/" element={<LandingPage />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/forgot-password" element={<ForgotPassword />} />
                   <Route path="/update-password" element={<UpdatePassword />} />
